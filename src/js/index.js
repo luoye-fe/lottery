598ca2edacc3def7610a20255999f7cae95b3e06
骆也 <842891024@qq.com>
2016-01-16 11:51:21 +0800

    use event

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,14 @@ var $ = require('jquery');
 // staff data
 var staff = require('./staff.js');
 
+var Event = require('./event.js');
+
+
+Event.on('choujiang',function(args){
+	console.log(args);
+})
+
+Event.trigger('choujiang',{
+	name:'asd',
+	id:'123',
+})
