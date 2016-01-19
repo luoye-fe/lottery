4b3dfc6c6b78b31c86fc6445d1aeee379f70a3b9
black <153238311@qq.co>
2016-01-19 12:11:36 +0800

    lliushaopeng

--- src/js/show.js
+++ src/js/show.js
@@ -3,12 +3,16 @@ var $ = require('jquery');
 var staff = require('../data/staff.json');
 
 var Event = require('./event.js');
-$('.start').click(function(){
-
-	Event.trigger('begin',{
+/*$('.start').on('click', function() {
+    alert(1)
+        Event.trigger('begin',{
 		type:"0"
 	})
-})
 
+});*/
+var oUl = document.createElement('ul');
+for(var i<0;i<50;i++){
+	var oLi = document.createElement('li');
+	oUl.appendChild(oLi);
 
-
+}
